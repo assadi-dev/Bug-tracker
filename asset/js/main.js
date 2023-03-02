553d6232a80e725b2f831c5f3c0acf31b6b35d52
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-03-02 11:26:36 +0100

    action deconnexion

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,4 +2,12 @@ document.addEventListener("DOMContentLoaded", (e) => {
   /** si presence de token dans le local storage**/
   let isLogged = getItem();
   if (isEmpty(isLogged)) location.replace("../../index.html");
+
+  /**Deconnexion du site */
+  const logout_btn = document.querySelector(".logout_btn");
+
+  logout_btn.onclick = () => {
+    clearItem();
+    location.replace("../../index.html");
+  };
 });
